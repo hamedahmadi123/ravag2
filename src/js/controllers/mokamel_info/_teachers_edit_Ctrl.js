90e11ec301591e9d465f0aadc13c40736cedfc0b
hamed <z.hamed.a@gmail.com>
2022-01-24 15:52:06 +0330

    image teacher

--- src/js/controllers/mokamel_info/_teachers_edit_Ctrl.js
+++ src/js/controllers/mokamel_info/_teachers_edit_Ctrl.js
@@ -146,7 +146,7 @@ app.controller('Theacher_edit', ['$scope', '$filter', '$http', function ($scope,
 
     $("#uploadFile1").on('fileuploaded', function (event, data) {
         // and check what's in both params
-        $scope.stu_image = data.response.data;
+        $scope.th_image = data.response.data;
     });
     $("#toggleOrient").on('change', function () {
         var val = $(this).prop('checked');
